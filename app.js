29137e0c34c3a34edd07b0ed0bbe67d2c2e7d9f7
master-dhanesh <dhanesh1296@gmail.com>
2024-06-19 10:05:31 +0530

    Express Generator Boilerplate

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
-// config dotenv
 require("dotenv").config({ path: "./.env" });
-const path = require("path");
-const express = require("express");
-const app = express();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-// connect to database
-require("./models/database").connectDB();
+// db connection
+const db = require("./models/database");
+db.connect();
+var userRoutes = require("./routes/user.routes");
 
-const userRoutes = require("./routes/user.routes");
+var app = express();
 
-// configure views
-app.set("view engine", "ejs");
+// view engine setup
 app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/user", userRoutes);
 
-// server
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running`);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
 });
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
+
+module.exports = app;
